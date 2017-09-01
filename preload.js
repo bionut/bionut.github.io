8f353cc4d3bd30c6ea1aeeb36ba1f1abb61335d9
Ionut Balcau <balcau.ionut@gmail.com>
2017-09-01 11:17:38 +0300

    Create preload.js

---
+++ preload.js
@@ -0,0 +1,58 @@
+
+var globalid = 1;
+function traceMethodCalls(obj, id=0) {
+    return new Proxy(obj, {
+        get: function (target, propKey, receiver) {
+            //I only want to intercept method calls, not property access
+            var propValue = target[propKey];
+            // console.log(`window#${id}.${propKey}(${[].slice.apply(arguments)})`);
+            if (typeof propValue != "function") {
+                console.log(`window#${id}.${propKey}`);
+                return propValue;
+            }
+            else {
+                return function () {
+                    console.log(`window#${id}.${propKey}(${[].slice.apply(arguments)})`);
+                    // return propValue.apply(this, arguments);
+                    return propValue.apply(target, arguments);
+                }
+            }
+        }
+    });
+}
+
+
+let oldWindowOpen = window.open;
+window.open = function (...args) {
+    console.log('window.open', ...args);
+    return traceMethodCalls(oldWindowOpen.apply(window, args), globalid++)
+}
+
+window.addEventListener('message', (event) => {
+    console.log(event);
+},true)
+
+function augment(obj, withFn) {
+    var name, fn;
+    for (name in obj) {
+        fn = obj[name];
+        if (typeof fn === 'function') {
+            obj[name] = (function(name, fn) {
+                var args = arguments;
+                return function() {
+                    withFn.apply(this, args);
+                    return fn.apply(this, arguments);
+                }
+            })(name, fn);
+        }
+    }
+}
+
+augment(window, function(name, fn) {
+    console.log("calling " + name);
+});
+
+
+
+let windowOpener = window.opener;
+if(window.opener) window.opener = traceMethodCalls(windowOpener, 'opener');
